cbfa936345a69259723851be5ce63194f6dd0f5b
Vlatko Kosturjak <kost@linux.hr>
2016-08-31 07:16:10 +0200

    ident fixes + small fixes

--- examples/backup-reports.rb
+++ examples/backup-reports.rb
@@ -2,22 +2,22 @@
 
 require 'nessus_rest'
 
-n=NessusREST::Client.new ({:url=>'https://localhost:8834', :username=>'user', :password=> 'password'})
+n=NessusREST::Client.new({:url=>'https://localhost:8834', :username=>'user', :password=> 'password'})
 
 formats=["nessus","csv","html"]
 folders_id=Hash.new
 
 sl["folders"].each do |f|
-        folders_id[f['id']]=f['name']
+  folders_id[f['id']]=f['name']
 end
 
 sl["scans"].each do |s|
-        puts "backing up: "+s["name"]+":"+s["uuid"].to_s 
-        formats.each do |format|
-		# fn = folder__name__scanid.format
-		outputfn=folders_id[s['folder_id']]+'__'+s['name']+'__'+s['id'].to_s+'.'+format
-                puts "-> Format: #{format"} Filename: #{outputfn}"
-                n.report_download_file(s['id'],format,outputfn)
-        end
-end
+  puts "backing up: "+s["name"]+":"+s["uuid"].to_s
+  formats.each do |format|
+    # fn = folder__name__scanid.format
+    outputfn=folders_id[s['folder_id']]+'__'+s['name']+'__'+s['id'].to_s+'.'+format
+    puts "-> Format: #{format} Filename: #{outputfn}"
+    n.report_download_file(s['id'],format,outputfn)
+  end # formats
+end # scans
 
