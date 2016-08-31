3c99f4025a27636a5ff4cb00de794a34c173a33b
Vlatko Kosturjak <kost@linux.hr>
2016-08-31 07:16:36 +0200

    remove space

--- examples/simple.rb
+++ examples/simple.rb
@@ -2,7 +2,7 @@
 
 require 'nessus_rest'
 
-n=NessusREST::Client.new ({:url=>'https://localhost:8834', :username=>'user', :password=> 'password'})
+n=NessusREST::Client.new({:url=>'https://localhost:8834', :username=>'user', :password=> 'password'})
 qs=n.scan_quick_template('basic','name-of-scan','localhost')
 scanid=qs['scan']['id']
 n.scan_wait4finish(scanid)
